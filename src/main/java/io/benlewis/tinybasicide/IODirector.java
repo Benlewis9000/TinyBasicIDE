9a4ab3a0b5e06526bd64e0662afbeb4e2f606849
Benlewis9000 <benjaminlewis9000@gmail.com>
2021-07-06 21:25:09 +0100

    Implemented IO director to stream process output to a TextArea.

---
+++ src/main/java/io/benlewis/tinybasicide/IODirector.java
@@ -0,0 +1,72 @@
+package io.benlewis.tinybasicide;
+
+import javafx.application.Platform;
+import javafx.scene.control.TextArea;
+
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class IODirector {
+
+    /**
+     * Redirect input from an InputStream to a given textarea asynchronously in realtime.
+     * @param src source of streamed input
+     * @param ta TextArea to output to
+     */
+    public static void redirectIO(InputStream src, TextArea ta) {
+
+        App app = App.getInstance();
+
+        EditorController controller = app.getEditorLoader().getController();
+
+        // Delay between executor schedules
+        final int DELAY = 20;
+        // Max lines displayed in the TextArea at any given time
+        final int LINES_TO_DISPLAY = 1000;
+        // Max amount of lines to accumulate before forcing an update to output
+        final int BATCH_LIMIT = 100;
+
+        // Read in lines, store in List
+        Scanner scanner = new Scanner(src);
+        List<String> lines = new LinkedList<>();
+
+        // Create execute
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+        // Schedule executor with anonymous Runnable
+        executor.scheduleWithFixedDelay(() -> {
+
+            // When input exhausted
+            if (!scanner.hasNextLine()){
+                // Unlock buttons and shutdown executor
+                controller.unlockButtons();
+                executor.shutdown();
+            }
+
+            int batchSize = 0;
+
+            // Add next line to lines
+            while(scanner.hasNextLine() && batchSize < BATCH_LIMIT) {
+                batchSize++;
+                lines.add(scanner.nextLine());
+                // If lines exceeds limit, remove oldest (first) line
+                if (lines.size() > LINES_TO_DISPLAY) {
+                    lines.remove(0);
+                }
+            }
+
+            // Queue task to run on the JavaFX application thread
+            Platform.runLater(() -> {
+                ta.setText(String.join(System.lineSeparator(), lines));
+                ta.setScrollTop(1000000);
+            });
+
+        }, 0, DELAY, TimeUnit.MILLISECONDS);
+    }
+
+}
